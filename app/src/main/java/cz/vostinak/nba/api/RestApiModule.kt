5ecd30e44e1c447f38d33bd69c07950a8e34f702
Petr Vostinak <petr.vostinak@gmail.com>
2024-11-21 12:29:35 +0100

    rest api module update - providers

--- app/src/main/java/cz/vostinak/nba/api/RestApiModule.kt
+++ app/src/main/java/cz/vostinak/nba/api/RestApiModule.kt
@@ -1,9 +1,16 @@
 package cz.vostinak.nba.api
 
+import com.google.gson.GsonBuilder
+import cz.vostinak.nba.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -11,32 +18,105 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object RestApiModule {
 
-    @Qualifier
-    @Retention(AnnotationRetention.BINARY)
-    annotation class BaseUrl
+    @Singleton
+    @Provides
+    fun nbaPlayersApi(
+        @MainRetrofit retrofit: Retrofit
+    ): NbaPlayersRestAPI {
+        return retrofit.create(NbaPlayersRestAPI::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun nbaTeamsApi(
+        @MainRetrofit retrofit: Retrofit
+    ): NbaTeamsRestAPI {
+        return retrofit.create(NbaTeamsRestAPI::class.java)
+    }
+
+    // ----------------------------------------- Base URL ------------------------------------------
 
     @Qualifier
     @Retention(AnnotationRetention.BINARY)
-    annotation class OkHttpClient
+    annotation class BaseUrl
 
     @Provides
     @BaseUrl
     fun provideBaseUrl() = "https://api.balldontlie.io"
 
+    // ----------------------------------- GsonConverterFactory ------------------------------------
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class GsonConverter
+
+    @Singleton
+    @Provides
+    @GsonConverter
+    fun provideGsonConverter(): GsonConverterFactory {
+        return GsonConverterFactory.create(
+            GsonBuilder()
+                .create()
+        )
+    }
+
+    // ----------------------------------------- Retrofit ------------------------------------------
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class MainRetrofit
+
+    @Singleton
+    @Provides
+    @MainRetrofit
+    fun provideRetrofit(
+        @BaseUrl baseUrl: String,
+        @GsonConverter gsonConverter: GsonConverterFactory,
+        @OkHttpClient httpClient: okhttp3.OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(gsonConverter)
+            .client(httpClient)
+            .build()
+    }
+
+    // --------------------------------------- Http Client -----------------------------------------
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class OkHttpClient
+
     @Singleton
     @Provides
     @OkHttpClient
-    fun provideOkHttpClient(): okhttp3.OkHttpClient = RestApi.httpClient
+    fun provideHttpClient(): okhttp3.OkHttpClient {
+        val httpClientBuilder = okhttp3.OkHttpClient.Builder()
 
-    @Singleton
-    @Provides
-    fun nbaPlayersApi(): NbaPlayersRestAPI {
-        return RestApi.restAdapter.create(NbaPlayersRestAPI::class.java)
+        httpClientBuilder.addInterceptor(Interceptor { chain ->
+            val builder = chain.request().newBuilder()
+
+            val request = builder
+                .header("Authorization", BuildConfig.API_KEY)
+                .build()
+
+            val response = chain.withReadTimeout(30, TimeUnit.SECONDS).proceed(request)
+            response
+        })
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        httpClientBuilder.addInterceptor(loggingInterceptor)
+
+        httpClientBuilder.connectTimeout(30, TimeUnit.SECONDS)
+        httpClientBuilder.readTimeout(30, TimeUnit.SECONDS)
+        httpClientBuilder.writeTimeout(30, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) {
+            httpClientBuilder.addInterceptor(LogRestApiForCrashInterceptor())
+        }
+
+        return httpClientBuilder.build()
     }
 
-    @Singleton
-    @Provides
-    fun nbaTeamsApi(): NbaTeamsRestAPI {
-        return RestApi.restAdapter.create(NbaTeamsRestAPI::class.java)
-    }
 }
