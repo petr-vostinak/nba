e17d86b31c3bc198f8fbe9a9c0740aa3936971d9
Petr Vostinak <petr.vostinak@gmail.com>
2025-02-14 23:47:10 +0100

    presentation module - preview fix

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    debugImplementation(libs.androidx.ui.tooling)
 
     // Compose
     implementation(platform(libs.androidx.compose.bom))
