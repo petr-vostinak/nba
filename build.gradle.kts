990de703f542ffc3b676d76eaa51beebf69ebcd5
Petr Vostinak <petr.vostinak@gmail.com>
2025-02-27 23:08:38 +0100

    plugins version catalog cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.devtools.ksp) apply false
     alias(libs.plugins.android.library) apply false
 }
